982ee83882bc5597d3ff808413bc37498c840a9d
handwoo24 <handwoo24@gmail.com>
2023-08-11 13:16:12 +0900

    :bug: fix timer error

--- src/initCv.ts
+++ src/initCv.ts
@@ -5,7 +5,7 @@ declare const cv: Cv
 
 const recursiveCv = (elapsedms: number, ms: number, fn: (cv: Cv) => void): void => {
   const diffms = elapsedms ? new Date().getTime() - elapsedms : 0
-  if (diffms) throw new Error('failed to initialize cv with timeout')
+  if (diffms > ms) throw new Error('failed to initialize cv with timeout')
   else if (typeof cv === 'undefined') {
     return debounce(recursiveCv, 100)(diffms, ms, fn)
   } else return fn(cv)
